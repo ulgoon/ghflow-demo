daf03891d81300db5bd2244aafd3a2984c4edb07
ulgoon <ulgoon89@gmail.com>
2022-05-05 13:50:49 +0900

    feat: Do fizzbuzz with if, for statements

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print('hello!')
+for i in range(1,10):
+    if i%15==0:
+        print('fizzbuzz')
+    elif i%3==0:
+        print('fizz')
+    elif i%5==0:
+        print('buzz')
+    else:
+        print(i)
